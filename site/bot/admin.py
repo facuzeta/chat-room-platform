9055e11370206bb86a55e31e9c6dab3f85f992c0
ValenValls <valentinvalls@hotmail.com>
2024-12-23 16:50:00 -0300

    removed system prompt from admin bot

--- site/bot/admin.py
+++ site/bot/admin.py
@@ -4,7 +4,7 @@ from bot.models import *
 # Register your models here.
 @admin.register(Bot)
 class BottAdmin(SimpleHistoryAdmin):
-    list_display = ("behaviour_nickname","chatroom_nickname","make_random_nickname_on_create", "model", "system_prompt", "reply_probability", "poll_time","know_own_nickname","use_current_topic", "use_all_chat_history","use_time_left_threshold","time_left_threshold", "empty_replies_enabled","temperature","max_tokens")
+    list_display = ("behaviour_nickname","chatroom_nickname","make_random_nickname_on_create", "model", "reply_probability", "poll_time","know_own_nickname","use_current_topic", "use_all_chat_history","use_time_left_threshold","time_left_threshold", "empty_replies_enabled","temperature","max_tokens")
     
 @admin.register(Argument)
 class ArgumentAdmin(SimpleHistoryAdmin):
